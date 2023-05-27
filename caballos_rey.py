1788366e266926b2bec44e2a5a1a3ac10f2531ae
Luis Rosero <luis.rosero@wiga.io>
2023-05-27 00:52:41 -0500

    Feat: funcion heuristica

--- caballos_rey.py
+++ caballos_rey.py
@@ -1,5 +1,4 @@
-Numlet = {1:"A",2:"B",3:"C",4:"D",5:"E",6:"F",7:"G",8:"H"}
-letNum = {"A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8}
+
 Cerrados = []
 Abiertos = []
 Eactual = []
@@ -9,21 +8,28 @@ def ValorHeuristica(CaballoActual,rey,vectorjaque):
     herustica = (abs( rey[0]["x"] - CaballoActual["x"] ) + abs( rey[0]["y"] -  CaballoActual["y"]) )* 10
     
 
-    if CaballoActual == Eactual:
+    # if CaballoActual == Eactual :
+        # ejecucion += 1
+    PMovimientos = movimientoCaballo(CaballoActual,rey,vectorjaque)
 
-        PMovimientos = movimientoCaballo(CaballoActual,rey,vectorjaque)
-
-        conjunto_PMovimientos = {tuple(d.items()) for d in PMovimientos}
-        conjunto_vectorjaque = {tuple(d.items()) for d in vectorjaque}
-
-        union = conjunto_PMovimientos & conjunto_vectorjaque
-
-        if len(union) > 0:
-            herustica -= len(union) * 10
+    conjunto_PMovimientos = {tuple(d.items()) for d in PMovimientos}
+    conjunto_vectorjaque = {tuple(d.items()) for d in vectorjaque}
+    # conjunto_rey = {tuple(d.items()) for d in rey}
 
 
-    if CaballoActual in vectorjaque:
+    union = conjunto_PMovimientos & conjunto_vectorjaque
+
+    if len(union) > 0:
+        herustica -= len(union) * 10
+
+    if rey[0] in PMovimientos:
         herustica +=50
+
+    if CaballoActual in vectorjaque :
+        herustica +=50
+
+
+    
 
 
     return herustica
@@ -36,46 +42,46 @@ def movimientoCaballo(caballo , rey , vectorjaque):
     global Abiertos, Eactual
 
     movimientos.append({"x":(caballo["x"])-1,"y":(caballo["y"])+2,
-                        "h": ValorHeuristica({"x":(caballo["x"])-1,"y":(caballo["y"])+2},rey,vectorjaque)
+                        # "h": ValorHeuristica({"x":(caballo["x"])-1,"y":(caballo["y"])+2},rey,vectorjaque)
                         })
     
     movimientos.append({"x":(caballo["x"])-2,"y":(caballo["y"])+1,
-                        "h": ValorHeuristica({"x":(caballo["x"])-2,"y":(caballo["y"])+1},rey,vectorjaque)
+                        # "h": ValorHeuristica({"x":(caballo["x"])-2,"y":(caballo["y"])+1},rey,vectorjaque)
                         }
                         )
     
     movimientos.append({"x":(caballo["x"]) -2,"y":(caballo["y"])-1,
-                        "h": ValorHeuristica({"x":(caballo["x"])-2,"y":(caballo["y"])-1},rey,vectorjaque)
+                        # "h": ValorHeuristica({"x":(caballo["x"])-2,"y":(caballo["y"])-1},rey,vectorjaque)
                         })
     
     movimientos.append({"x":(caballo["x"])-1,"y":(caballo["y"])-2,
-                        "h": ValorHeuristica({"x":(caballo["x"])-1,"y":(caballo["y"])-2},rey,vectorjaque)
+                        # "h": ValorHeuristica({"x":(caballo["x"])-1,"y":(caballo["y"])-2},rey,vectorjaque)
                         })
     
     movimientos.append({"x":(caballo["x"])+1,"y":(caballo["y"])-2,
-                        "h": ValorHeuristica({"x":(caballo["x"])+1,"y":(caballo["y"])-2},rey,vectorjaque)
+                        # "h": ValorHeuristica({"x":(caballo["x"])+1,"y":(caballo["y"])-2},rey,vectorjaque)
                         })
     
     movimientos.append({"x":(caballo["x"])+2,"y":(caballo["y"])-1,
-                        "h": ValorHeuristica({"x":(caballo["x"])+2,"y":(caballo["y"])-1},rey,vectorjaque)
+                        # "h": ValorHeuristica({"x":(caballo["x"])+2,"y":(caballo["y"])-1},rey,vectorjaque)
                         })
     
     movimientos.append({"x":(caballo["x"])+2,"y":(caballo["y"])+1,
-                        "h": ValorHeuristica({"x":(caballo["x"])+2,"y":(caballo["y"])+1},rey,vectorjaque)
+                        # "h": ValorHeuristica({"x":(caballo["x"])+2,"y":(caballo["y"])+1},rey,vectorjaque)
                         })
     
     movimientos.append({"x":(caballo["x"])+1,"y":(caballo["y"])+2,
-                        "h": ValorHeuristica({"x":(caballo["x"])+1,"y":(caballo["y"])+2},rey,vectorjaque)
+                        # "h": ValorHeuristica({"x":(caballo["x"])+1,"y":(caballo["y"])+2},rey,vectorjaque)
                         })
     
     movimientos = list(filter(lambda elemento: (elemento["x"] < 9 and elemento["x"] > 0)  and  (elemento["y"] < 9 and elemento["y"] > 0 ) , movimientos))
                     
-    Abiertos.extend(movimientos)
-    Abiertos.sort(key = lambda x: x["h"], reverse=True)
+    # Abiertos.extend(movimientos)
+    # Abiertos.sort(key = lambda x: x["h"], reverse=True)
 
-    Eactual = min(Abiertos, key= lambda x: x["h"])
+    # Eactual = min(Abiertos, key= lambda x: x["h"])
 
-    print(Abiertos)
+    # print(Abiertos)
 
     return movimientos
 
@@ -86,16 +92,13 @@ def orderVector(caballos,rey,vector_jaque):
     aux = []
 
     for i in caballos:
-
-        Eactual = i
-        Cerrados = i
         
         if i in vector_jaque:
             aux.append({"x":i["x"], "y":i["y"], "h": ValorHeuristica(i , rey , vector_jaque)})
         else:
             resultado.append({"x":i["x"], "y":i["y"], "h": ValorHeuristica(i , rey , vector_jaque)})
 
-    resultado.sort(key = lambda x: x["h"], reverse= True)
+    resultado.sort(key = lambda x: x["h"],reverse=True)
     resultado = aux + resultado
 
     return resultado
@@ -137,6 +140,9 @@ def imprimirTablero(caballos, rey):
     
 def traductor(posicion):
 
+    Numlet = {1:"A",2:"B",3:"C",4:"D",5:"E",6:"F",7:"G",8:"H"}
+    letNum = {"A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8}
+
     respuesta = []
     for i in posicion:
         respuesta.append({"x":(letNum.get(i["x"])),"y":i["y"]})
@@ -148,10 +154,10 @@ def main():
     # Caballos = []
     # Rey = []
     # vector_jaque = []
-    # Caballos = [{"x": "H", "y": 6},{"x": "F", "y": 2},{"x": "B", "y": 3},
-    #             {"x": "B", "y": 8}] 
-    Caballos = [{"x": "H", "y": 6}] 
-    Rey = [{"x": "A", "y": 4}]
+    Caballos = [{"x": "A", "y": 8},{"x": "H", "y": 7},{"x": "G", "y": 1},
+                {"x": "H", "y": 1}] 
+    # Caballos = [{"x": "H", "y": 1}] 
+    Rey = [{"x": "B", "y": 1}]
     # for i in range(1,5):
     #     print(f"\nIngrese las posiciones del caballo {i}")
     #     Caballos.append({"x":input("Eje x: "),
@@ -172,9 +178,36 @@ def main():
     vector_jaque = vectorJaque(Rey)
 
     Caballos = orderVector(Caballos,Rey,vector_jaque)
+
+    print(Caballos)
     
-    # for i,elemento in enumerate(Caballos):
-    #     movimientoCaballo(Caballos[i],Rey,vector_jaque)
+    for i in Caballos:
+
+        # moves = movimientoCaballo(i,Rey,vector_jaque)
+        Eactual = i
+
+        while True :
+
+            movimientosParciales = movimientoCaballo(Eactual,Rey,vector_jaque)
+            conjunto_moves = {tuple(d.items()) for d in movimientosParciales}
+            conjunto_vectorjaque = {tuple(d.items()) for d in vector_jaque}
+            union = conjunto_moves & conjunto_vectorjaque
+
+            for j in movimientosParciales:
+                j["h"] = ValorHeuristica(j,Rey,vector_jaque)
+            
+            print(movimientosParciales)
+
+            nodoPadre = Eactual
+            menor = min(movimientosParciales, key= lambda x:x["h"])
+            Eactual = menor if menor["h"] < Eactual["h"] else Eactual
+            
+            print(Eactual)
+            if Eactual["h"] <= nodoPadre["h"] and len(union) > 0 : break
+
+
+
+    # print(movimientosParciales)
 
     print(Caballos)
 
